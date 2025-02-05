361db8e68236da54776459f3a44868ea8685d5ef
Harley Clair <60947601+harley-dane@users.noreply.github.com>
2025-02-05 10:03:00 +0100

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,22 @@
+const data = {
+    items: [
+        {
+            id: 1,
+            text: 'Buy vitamins',
+        },
+        {
+            id: 2,
+            text: 'Learn JavaScript',
+        },
+        {
+            id: 3,
+            text: 'Wash dishes',
+        },
+        {
+            id: 4,
+            text: 'Call mum',
+        },
+    ],
+};
+
+export default data;
---
+++ src/dom.js
@@ -0,0 +1,9 @@
+const dom = {
+    input: document.getElementById('todo-input'),
+    submitBtn: document.querySelector('.submit-btn'),
+    error: document.querySelector('.error'),
+    itemsList: document.querySelector('.todos-list'),
+    clearAllBtn: document.querySelector('.clear-btn'),
+};
+
+export default dom;
